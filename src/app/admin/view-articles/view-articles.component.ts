41b09b6173aecd95424f048000aca8a80aa1206b
sandip <mrsandipkumar63@gmail.com>
2023-12-30 10:58:39 +0530

    View article Done

--- src/app/admin/view-articles/view-articles.component.ts
+++ src/app/admin/view-articles/view-articles.component.ts
@@ -9,12 +9,14 @@ import { ArticleService } from 'src/app/services/article.service';
 export class ViewArticlesComponent implements OnInit {
   articles: any = [];
 
-  userId: number = 1;
+  userId: number = 102;
 
   content: string =
     'Your long content goes here. This is just a sample text for demonstration.Your long content goes here. This is just a sample text for demonstration.';
-  truncatedContent: string = '';
-  showSeeMore: boolean = true;
+
+  // Assuming articles have properties like 'title' and 'content'
+  showSeeMore: boolean[] = [];
+  truncatedContent: string[] = [];
 
   //dropdown item
 
@@ -35,23 +37,46 @@ export class ViewArticlesComponent implements OnInit {
       }
     );
     this.dropDownItem = new Array(this.articles.length).fill(false);
+    this.showSeeMore = new Array(this.articles.length).fill(true);
     this.truncateContent();
   }
 
   truncateContent() {
     const maxLength = 50; // Set your desired maximum length
-    if (this.content.length > maxLength) {
-      this.truncatedContent = this.content.substring(0, maxLength) + '...';
-    } else {
-      this.truncatedContent = this.content;
-      this.showSeeMore = false;
-    }
+
+    // Truncate content for each article
+    this.articles.data.forEach((article: any, index: number) => {
+      if (article.content.length > maxLength) {
+        this.truncatedContent[index] =
+          article.content.substring(0, maxLength) + '...';
+      } else {
+        this.truncatedContent[index] = article.content;
+        this.showSeeMore[index] = false;
+      }
+    });
   }
 
-  public toggleContent() {
-    this.truncatedContent = this.content;
-    this.showSeeMore = false;
+  // public toggleContent() {
+  //   this.truncatedContent = this.content;
+  //   this.showSeeMore = false;
+  // }
+  public toggleContent(index: number) {
+    // Toggle "See More" for the specific article
+    this.showSeeMore[index] = !this.showSeeMore[index];
+
+    // If showing more, set full content; otherwise, truncate
+    this.truncatedContent[index] = this.showSeeMore[index]
+      ? this.articles.data[index].content
+      : this.truncate(this.articles.data[index].content);
   }
+
+  private truncate(content: string): string {
+    const maxLength = 50; // Set your desired maximum length
+    return content.length > maxLength
+      ? content.substring(0, maxLength) + '...'
+      : content;
+  }
+
   showDropDownItem(event: MouseEvent, index: number) {
     event.stopPropagation(); // Prevent the click event from propagating to the document
     this.dropDownItem[index] = !this.dropDownItem[index];
